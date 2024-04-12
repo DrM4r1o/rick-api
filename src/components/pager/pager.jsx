5773417761d3e1e4450fe2a03de7a987320c6548
Mario Esparza Pérez <mesparzp@emeal.nttdata.com>
2024-04-12 09:59:13 +0200

    feat: page buttons working correctly

--- src/components/pager/pager.jsx
+++ src/components/pager/pager.jsx
@@ -1,24 +1,93 @@
 import './pager.css'
 
-export default function Pager({page, lastPage, handlePage}) {
+import { useState } from 'react'
+import { useEffect } from 'react'
 
-    function nextPage() {
+export default function Pager({ page, lastPage, handlePage }) {
+    const [pageDivider, setPageDivider] = useState(1)
+    const [numberOfButtons, setNumberOfButtons] = useState(5)
+
+    useEffect(() => {
+        if(page === 1) {
+            setPageDivider(page)
+            setNumberOfButtons(5)
+        }
+        if(page === lastPage) {
+            const newPageDivider = getPrevDivder(lastPage) + 1
+            setPageDivider(newPageDivider)
+            setNumberOfButtons((lastPage - newPageDivider) + 1)
+        }
+    }, [page])
+
+    const updatePageDivider  = (newValue, { advance }) => {
+        let difference = lastPage - newValue
+        let isDivisible = false
+        let newPageDivider = 1
+        
+        if(advance) {
+            isDivisible = page % 5 === 0
+            newPageDivider = newValue
+        } else {
+            isDivisible = newValue % 5 === 0
+            newPageDivider = page - 5
+            difference =  lastPage - (page - 5)
+        }
+
+        if(isDivisible) {
+            setPageDivider(newPageDivider)
+            setNumberOfButtons(difference < 5 ? difference + 1 : 5)
+        }
+    }
+
+    const nextPage = () => {
         if (page < lastPage) {
+            updatePageDivider(page + 1, { advance: true })
             handlePage(page + 1)
         }
     }
 
-    function prevPage() {
+    const prevPage = () => { 
         if (page > 1) {
+            updatePageDivider(page - 1, { advance: false })
             handlePage(page - 1);
         }
     }
 
+    const getPrevDivder = (page) => {
+        for (let i = page; i > 0; i--) {
+            if(i % 5 === 0) return i
+        }
+    }
+
+    const toFirstPage = () => handlePage(1)
+    const toLastPage = () => handlePage(lastPage)
+    
+
+    const ButtonsPages = () => {
+        return <>
+            {Array.from({ length: numberOfButtons }, (_, i) => (
+                <button
+                    style={{ backgroundColor: pageDivider + i === page && "#E6D82D" }}
+                    key={page + i}
+                    onClick={() => handlePage(pageDivider + i)}
+                >{pageDivider + i}</button>
+            ))}
+        </>
+    }
+
     return (
         <div className='pager'>
-            <button onClick={prevPage} disabled={page <= 1}>Prev</button>
-            <p>{page}/{lastPage}</p>
-            <button onClick={nextPage} disabled={page >= lastPage}>Next</button>
+            <div>
+                <button onClick={toFirstPage}>First</button>
+                <button onClick={prevPage} disabled={page <= 1}>Prev</button>
+            </div>
+            <div>
+                <ButtonsPages />
+            </div>
+            <div>
+                <button onClick={nextPage} disabled={page >= lastPage}>Next</button>
+                <button onClick={toLastPage}>Last</button>
+            </div>
         </div>
     )
 }
--- src/utils/handlers.util.js
+++ src/utils/handlers.util.js
@@ -1,1 +1,2 @@
 export const getRating = () => Math.ceil(Math.random() * 9)
+export const getRandomId = () => Math.ceil(Math.random() * 1000).toString()
